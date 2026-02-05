d00901abe122b1f75cbc9616fd13983e83b87a09
Sadykov <sadykovismoil95@gmail.com>
2026-02-05 18:56:06 +0500

    Fix axios refreshFun to use backend URL

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,50 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosRequest = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-
 axiosRequest.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('access')
+    const token = localStorage.getItem("access");
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
-  (error) => error
-)
+  (error) => error,
+);
 
 axiosRequest.interceptors.response.use(
   (response) => response,
   async (error) => {
     if (error.response.status == 401) {
-      const res = await refreshFun()
+      const res = await refreshFun();
 
       if (res) {
-        axiosRequest(error.config)
-      }  
-      else {
-        localStorage.clear()
-        window.location.pathname = "/guest"
+        axiosRequest(error.config);
+      } else {
+        localStorage.clear();
+        window.location.pathname = "/guest";
       }
     }
-    console.error('API error:', error.response?.data || error.message);
+    console.error("API error:", error.response?.data || error.message);
     return Promise.reject(error);
-  }
+  },
 );
 
-
 const refreshFun = async () => {
-  const refresh = localStorage.getItem('refresh')
+  const refresh = localStorage.getItem("refresh");
   try {
-    const { data } = await axios.post("/api/users/token/refresh/", {refresh: refresh})
-    localStorage.setItem("access", data.access)
-    return data.access
+    const { data } = await axiosRequest.post("/api/users/token/refresh/", {
+      refresh,
+    });
+    localStorage.setItem("access", data.access);
+    return data.access;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 export default axiosRequest;
